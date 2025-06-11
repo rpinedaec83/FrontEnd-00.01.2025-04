448b9c27bf8054aa57596e8568bdbff01fed8f5a
Roberto Pineda <roberto.pineda@convergia.com>
2025-06-10 21:35:00 -0500

    Clase 10062025 segunda parte

--- Semana10Sesion01/d19423/js/script.js
+++ Semana10Sesion01/d19423/js/script.js
@@ -1,29 +1,58 @@
 console.log("Inicio de la aplicacion");
 
-let strObjJSON =`
+// let strObjJSON =`
 
-{
-  "nombre" : "Roberto",
-  "apellido" : "Pineda",
-  "edad" : 41,
-  "peso": 75.85
+// {
+//   "nombre" : "Roberto",
+//   "apellido" : "Pineda",
+//   "edad" : 41,
+//   "peso": 75.85
+// }
+
+
+// `;
+
+// let objPersona = 
+// {
+//   nombre: "David",
+//   apellido: "Lopez",
+//   edad: 35,
+//   peso: 69.90  
+// };
+
+// console.log(JSON.stringify(objPersona));
+
+// console.log(JSON.parse(strObjJSON))
+
+// let objPerseado = JSON.parse(strObjJSON);
+
+// console.log(objPerseado.edad);
+
+
+// localStorage.setItem("persona", "Roberto");
+
+// $("#nombre").text(localStorage.getItem("persona"));
+
+// localStorage.removeItem("persona");
+
+
+//  sessionStorage.setItem("idCliente", 123456789)
+
+// console.log(sessionStorage.getItem("idCliente"))
+
+// localStorage.setItem("usuario", JSON.stringify(objPersona))
+
+
+class Gundam {
+  constructor(nombre,descripcion,imagen, escala, isCustom=false, custom= "" ){
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.imagen = imagen;
+    this.escala = escala;
+    this.isCustom = isCustom;
+    this.custom = custom;
+  }
+  toJSON(){
+    return JSON.stringify(this);
+  }
 }
-
-
-`;
-
-let objPersona = 
-{
-  nombre: "David",
-  apellido: "Lopez",
-  edad: 35,
-  peso: 69.90  
-};
-
-console.log(JSON.stringify(objPersona));
-
-console.log(JSON.parse(strObjJSON))
-
-let objPerseado = JSON.parse(strObjJSON);
-
-console.log(objPerseado.edad)
