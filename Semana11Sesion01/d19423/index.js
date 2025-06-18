707c943fc99073edf5e82812c6b76ccaf924d96b
Roberto Pineda <roberto.pineda@convergia.com>
2025-06-17 22:24:13 -0500

    Clase 17062025 Segunda parte

--- Semana11Sesion01/d19423/index.js
+++ Semana11Sesion01/d19423/index.js
@@ -14,10 +14,96 @@ console.log("Hola desde NodeJS")
 // }).listen(3000);
 
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
-app.get("/", (req,res)=>res.send("Hola desde express"));
-app.get("/hola/:nombre",(req,res)=>{
+const axios = require('axios');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => res.send("Hola desde express"));
+app.get("/hola/:nombre", (req, res) => {
     let nombre = req.params.nombre
     res.send(`Hola ${nombre}`);
+});
+
+
+app.get('/query', (req, res) => {
+    let pregunta = req.query;
+    console.log(pregunta);
+
+    res.send(`Datos personales: Nombre: ${pregunta.nombre} Apellido: ${pregunta.apellido} Edad: ${pregunta.edad}`)
 })
+
+let arrUsuario = []
+app.post("/crearusuario", (req, res) => {
+    let usuario = req.body;
+    console.log(usuario)
+    arrUsuario.push(usuario);
+    console.log(arrUsuario)
+    res.status(201).send("Usuario creado")
+})
+
+app.put("/usuario/:nombre", (req, res) => {
+    let nombre = req.params.nombre;
+    console.log(nombre);
+
+    res.status(202).send({ message: "Se actualizo correctamente" })
+})
+
+
+app.get("/rick-and-morty", (req, res) => {
+
+    //https://rickandmortyapi.com/api/character
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://rickandmortyapi.com/api/character',
+        headers: {}
+    };
+
+    axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data.info.count));
+            res.send({ message: JSON.stringify(response.data.info.count) })
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send({ message: error })
+        });
+
+
+})
+
+
+app.get("/rick-and-morty/:id", (req, res) => {
+
+    let id = req.params.id;
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://rickandmortyapi.com/api/character/'+id,
+        headers: {}
+    };
+
+    axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+            res.send(response.data)
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send({ message: error })
+        });
+
+
+})
+
+app.delete("/usuario/:nombre", (req, res) => {
+    let nombre = req.params.nombre;
+    console.log(nombre);
+
+    res.status(202).send({ message: "Se borro correctamente" })
+})
+
 app.listen(3000)
