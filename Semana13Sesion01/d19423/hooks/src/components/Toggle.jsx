020d1d5b618a1a8fcc508d24bb420e8a988a5c36
Roberto Pineda <roberto.pineda@convergia.com>
2025-07-01 20:50:16 -0500

    Clase 01072025 segunda Parte

--- Semana13Sesion01/d19423/hooks/src/components/Toggle.jsx
+++ Semana13Sesion01/d19423/hooks/src/components/Toggle.jsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Toggle(){
     const [activo, setActivo]=useState(false);
+
+    useEffect(()=>{
+        
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+            })
+      //  }
+    },[]);
+
     return(
         <button
             onClick={()=>setActivo(!activo)}>
