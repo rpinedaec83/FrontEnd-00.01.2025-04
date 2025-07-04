737a1d18f78110f78f58ae838554390c76d4311b
Roberto Pineda <roberto.pineda@convergia.com>
2025-07-03 21:01:06 -0500

    Clase 03072025 segunda parte

--- Semana13Sesion02/d19423/router-bootstrap/src/main.jsx
+++ Semana13Sesion02/d19423/router-bootstrap/src/main.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
